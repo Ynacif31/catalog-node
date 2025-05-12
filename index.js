aea4cdd35a3afcd5abbec2201d926d813dc21847
Ygor Nacif <ygor.nacif00@gmail.com>
2025-05-12 10:07:43 -0300

    configurando express e bootstrap

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-// Importando o express
+// Imports
 const express = require("express");
+const mysql = require("mysql2");
+const { engine } = require("express-handlebars");
 
 // Inicializando o express
 const app = express();
 
+// adcionar bootstrap
+app.use('/bootstrap', express.static('/node_modules/bootstrap/dist'));
+
+// Configuração do banco de dados
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "curso-fullstack",
+});
+
+// Configuração do handlebars
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+// Conectando ao banco de dados
+db.connect((err) => {
+  if (err) {
+    console.log("Erro ao conectar ao banco de dados");
+  } else {
+    console.log("Conectado ao banco de dados");
+  }
+});
+
 // Criando uma rota
 app.get("/", (req, res) => {
-  res.write("Olá Mundo");
-  res.end();
+  res.render('formulario');
 });
 
 // Iniciando o servidor
 app.listen(8080, () => {
-  console.log("Servidor rodando na porta 3000");
+  console.log("Servidor rodando na porta 8080");
 });
